1e2f7f65c9fade9d6112e6efe97676551cf0d346
Lasse Jul-Larsen <ljl@enonic.com>
2012-12-06 12:41:34 +0100

    imageresult

---
+++ app/controllers/ImageResult.java
@@ -0,0 +1,12 @@
+package controllers;
+
+import models.ImageEntry;
+
+public class ImageResult
+{
+    public ImageEntry entry;
+    public ImageResult( final ImageEntry entry )
+    {
+        this.entry = entry;
+    }
+}
